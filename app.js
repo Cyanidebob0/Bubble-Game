f96f74db346d9351ca5a74c1b5b84b6a019e09a5
bhuvan <bhuvanannappa@gmail.com>
2024-09-06 21:28:43 +0530

    little more left

---
+++ app.js
@@ -0,0 +1,49 @@
+let timerVar=20;
+let rand=0;
+let panel=document.querySelector("#inner-panel2");
+let hitVar=0;
+let score=0;
+
+let hitVal =()=>{
+    hitVar=Math.floor(10 * Math.random())
+    document.querySelector("#hit").textContent=hitVar;
+}
+
+let createBubble=()=>{
+    var bubbles="";
+    for ( var i =0; i<170;i++){ 
+        rand= Math.floor(10 * Math.random());
+        bubbles +=`<div id="bubble">${rand}</div>`;
+    };
+    document.querySelector("#inner-panel2").innerHTML=bubbles;
+}
+
+let runTimer=()=>{
+    let interval=setInterval(()=>{
+        if (timerVar <= 0) {
+            clearInterval(interval); 
+        }
+        document.querySelector("#timer").textContent=`${timerVar}`;
+        timerVar--;
+    },1000)
+};
+
+panel.addEventListener("click",(e)=>{
+    let target = e.target;
+    let ClickedNum=Number(target.innerText);
+    if(ClickedNum === hitVar){
+        document.querySelector("#Score").textContent=`${++score}`;
+    }
+    hitVal();
+    createBubble();
+})
+
+hitVal();
+createBubble();
+
+runTimer();
+
+
+
+
+
